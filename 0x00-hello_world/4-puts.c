7f472963ab8ce2159064e618cf0892156016e6e9
Omotola Coker <omotola.coker.o@gmail.com>
2022-09-08 20:02:09 -0700

    fixed unterminated comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
-* main - print text in put function
-*
-* program should always return 0 after printing text
-* Return: 0
+ * main - print text in put function
+ * program should always return 0 after printing text
+ * Return: 0
+ */
 int main(void)
 {
  	puts("\"Programming is like building a multilingual puzzle");
