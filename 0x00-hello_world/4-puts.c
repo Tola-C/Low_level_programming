f267f38f3d5f1fd63b5fb562b2c6e0b1bb9b590b
Omotola Coker <omotola.coker.o@gmail.com>
2022-09-08 19:54:26 -0700

    write C program that prints exact text provided

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+* main - print text in put function
+*
+* program should always return 0 after printing text
+* Return: 0
+int main(void)
+{
+ 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
