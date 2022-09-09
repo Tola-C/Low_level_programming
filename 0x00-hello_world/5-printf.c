05ed31c7785808bf1f2beaa7642a11f29f1972cc
Omotola Coker <omotola.coker.o@gmail.com>
2022-09-08 20:12:23 -0700

    changed put to printf as required

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - print text in put function
+ * main - print text in printf function
  * program should always return 0 after printing text
  * Return: 0
  */
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
