687f758316c3e2aee7d371803b3c710796cc0e05
Omotola Coker <omotola.coker.o@gmail.com>
2022-09-08 20:54:22 -0700

    add program that prints prescribed text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print text in printf function
+ * program should always return 0 after printing text
+ * Return: 0
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n;
+	write(2,str1,59)
+	return (1);
+}
