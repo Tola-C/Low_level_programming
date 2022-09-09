bedb4a62208c25d56cb928f4151a33dfe020c1f6
Omotola Coker <omotola.coker.o@gmail.com>
2022-09-08 20:09:34 -0700

    add program that prints out another prescribed text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print text in put function
+ * program should always return 0 after printing text
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
