3cc1fb1db8dfd5cad7c03b5d69069463af7cf34f
Omotola <omotola.coker.o@gmail.com>
2023-02-26 14:34:14 -0800

    Update code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print all single digit base 10 numbers using putchar
+ * parameter: ch
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+	int ch;
+
+	for (ch = '0'; ch < '10'; ++ch)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
