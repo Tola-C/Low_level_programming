bf3a3254e4b82d18d640b42da89d0c610ba1fc4a
Omotola <omotola.coker.o@gmail.com>
2023-02-26 11:25:12 -0800

    Add source code to print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Use putchar to print letters of the alphabet in lowercase
+ * parameter: ch
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ++ch)
+	{
+	putchar(ch);
+	putchar("\n");
+	}
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-positive_or_negative.c uses if statements to print whether the number storned in the variable n is positive or negative
 1-last_digit.c is a source code to print the last digit of the number stored in the variable n
+2-print_alphabet.c is a source code that uses putchar function to print the alphabet
