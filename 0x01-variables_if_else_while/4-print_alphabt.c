839f9f92adc266ec3fbec5db780f7c9f268f44b9
Omotola <omotola.coker.o@gmail.com>
2023-02-26 13:30:56 -0800

    Add code to print all lowercase letters except e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Use putchar to print letters in lowercase except e & q
+ * parameter: ch
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+	if (ch != 'e' && ch != 'q')
+	{
+	putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 1-last_digit.c is a source code to print the last digit of the number stored in the variable n
 2-print_alphabet.c is a source code that uses putchar function to print the alphabet
 3-print_alphabets.c is a source code to print the lowercase alphabet followed by upper case
+4-print_alphabt.c is a code that prints all letters in lowercase except e & q
